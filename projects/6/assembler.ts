6f7b384fd36dd737a9a941502ef3a7126e8fad36
Thomas Cleary <thomasclearydev@gmail.com>
2024-03-24 20:35:27 +0800

    Project 6: First parse

--- projects/6/assembler.ts
+++ projects/6/assembler.ts
@@ -1,6 +1,32 @@
 import { readFileSync } from "fs";
 
-type Result<T> = ({ success: true } & T) | { success: false; message: string | undefined };
+type Result<T extends Record<PropertyKey, unknown>> = ({ success: true } & T) | { success: false; message: string };
+
+const PREDEFINED_SYMBOLS = [
+  ["R0", 0],
+  ["R1", 1],
+  ["R2", 2],
+  ["R3", 3],
+  ["R4", 4],
+  ["R5", 5],
+  ["R6", 6],
+  ["R7", 7],
+  ["R8", 8],
+  ["R9", 9],
+  ["R10", 10],
+  ["R11", 11],
+  ["R12", 12],
+  ["R13", 13],
+  ["R14", 14],
+  ["R15", 15],
+  ["SP", 0],
+  ["LCL", 1],
+  ["ARG", 2],
+  ["THIS", 3],
+  ["THAT", 4],
+  ["SCREEN", 16384],
+  ["KBD", 24576],
+] as const satisfies [string, number][];
 
 const getAssemblyFilePath = (): Result<{ filePath: string }> => {
   const filePath = process.argv[2];
@@ -13,26 +39,51 @@ const getAssemblyFilePath = (): Result<{ filePath: string }> => {
   }
 
   if (!filePath.endsWith(".asm")) {
-    return {
-      success: false,
-      message: `error: file type of ${filePath} is not .asm`,
-    };
+    return { success: false, message: `error: file type of ${filePath} is not .asm` };
   }
 
   return { success: true, filePath };
 };
 
-const getAssemblyProgram = (filePath: string): Result<{ assemblyProgram: string }> => {
+const getAssemblyProgram = (filePath: string): Result<{ assemblyProgram: readonly string[] }> => {
   try {
-    const assemblyProgram = readFileSync(filePath).toString().trim();
-
-    // TODO: convert string into a representation that will be easier to parse
-    // TODO: remove comments / empty lines etc
-
-    return { success: true, assemblyProgram };
-  } catch (e) {
+    return {
+      success: true,
+      assemblyProgram: readFileSync(filePath)
+        .toString()
+        .trim()
+        .split("\n")
+        .map((line) => line.trim().replaceAll(" ", ""))
+        .filter((line) => !!line && !line.startsWith("//")),
+    };
+  } catch {
     return { success: false, message: `error: unable to read file ${filePath}` };
   }
+};
+
+const getSymbolTable = (assemblyProgram: readonly string[]): Result<{ symbolTable: ReadonlyMap<string, number> }> => {
+  const symbolTable = new Map<string, number>(PREDEFINED_SYMBOLS);
+
+  let instructionCount = -1;
+
+  for (const line of assemblyProgram) {
+    if (line.startsWith("(")) {
+      const label = line.replace("(", "").replace(")", "");
+
+      if (PREDEFINED_SYMBOLS.map(([key, _]): string => key).includes(label)) {
+        return { success: false, message: `error: predefined symbol ${label} cannot be used as a label` };
+      }
+      if (symbolTable.has(label)) {
+        return { success: false, message: `error: label ${label} is defined more than once` };
+      }
+
+      symbolTable.set(label, instructionCount + 1);
+    } else {
+      instructionCount++;
+    }
+  }
+
+  return { success: true, symbolTable };
 };
 
 const main = () => {
@@ -53,10 +104,19 @@ const main = () => {
 
   const { assemblyProgram } = assemblyProgramResult;
   console.log(assemblyProgram);
+  const symbolTableResult = getSymbolTable(assemblyProgram);
 
-  // TODO: convert assemblyProgram to binary hack instructions
+  if (!symbolTableResult.success) {
+    console.log(symbolTableResult.message);
+    return;
+  }
 
-  // TODO: write binary hack instruction to ./path/to/pogram.hack
+  const { symbolTable } = symbolTableResult;
+  console.log(symbolTable);
+
+  // TODO: convert assembly program to binary hack instructions
+
+  // TODO: write binary hack instructions to a new file ./path/to/program.hack
 };
 
 main();
