7f3a8db0ab16018ec826aa1a9fe52774f8d3c251
Thomas Cleary <thomasclearydev@gmail.com>
2024-03-26 17:00:39 +0800

    Project 6: Add line numbers to error messages

--- projects/6/assembler.ts
+++ projects/6/assembler.ts
@@ -1,87 +1,28 @@
 import { readFileSync, writeFileSync } from "fs";
+import {
+  COMP_CODES,
+  C_INSTRUCTION_PREFIX,
+  DEST_CODES,
+  JUMP_CODES,
+  MEMORY_COMP_CODE,
+  PREDEFINED_SYMBOLS,
+  VARIABLE_ADDRESS_START,
+} from "./constants.js";
 
 type Result<T extends Record<PropertyKey, unknown>> = ({ success: true } & T) | { success: false; message: string };
-
-const PREDEFINED_SYMBOLS = {
-  R0: 0,
-  R1: 1,
-  R2: 2,
-  R3: 3,
-  R4: 4,
-  R5: 5,
-  R6: 6,
-  R7: 7,
-  R8: 8,
-  R9: 9,
-  R10: 10,
-  R11: 11,
-  R12: 12,
-  R13: 13,
-  R14: 14,
-  R15: 15,
-  SP: 0,
-  LCL: 1,
-  ARG: 2,
-  THIS: 3,
-  THAT: 4,
-  SCREEN: 16384,
-  KBD: 24576,
-} as const;
-
-const C_INSTRUCTION_PREFIX = 0b1110_0000_0000_0000;
-const MEMORY_COMP_CODE = 0b0001_0000_0000_0000;
-
-const DEST_CODES = {
-  A: 0b10_0000,
-  D: 0b01_0000,
-  M: 0b00_1000,
-} as const;
-
-const COMP_CODES = {
-  "0": 0b1010_1000_0000,
-  "1": 0b1111_1100_0000,
-  "-1": 0b1110_1000_0000,
-  D: 0b0011_0000_0000,
-  A: 0b1100_0000_0000,
-  M: 0b1100_0000_0000,
-  "!D": 0b0011_0100_0000,
-  "!A": 0b1100_0100_0000,
-  "!M": 0b1100_0100_0000,
-  "-D": 0b0011_1100_0000,
-  "-A": 0b1100_1100_0000,
-  "-M": 0b1100_1100_0000,
-  "D+1": 0b0111_1100_0000,
-  "A+1": 0b1101_1100_0000,
-  "M+1": 0b1101_1100_0000,
-  "D-1": 0b0011_1000_0000,
-  "A-1": 0b1100_1000_0000,
-  "M-1": 0b1100_1000_0000,
-  "D+A": 0b0000_1000_0000,
-  "D+M": 0b0000_1000_0000,
-  "D-A": 0b0100_1100_0000,
-  "D-M": 0b0100_1100_0000,
-  "A-D": 0b0001_1100_0000,
-  "M-D": 0b0001_1100_0000,
-  "D&A": 0b0000_0000_0000,
-  "D&M": 0b0000_0000_0000,
-  "D|A": 0b0101_0100_0000,
-  "D|M": 0b0101_0100_0000,
-} as const;
-
-const JUMP_CODES = {
-  JGT: 0b001,
-  JEQ: 0b010,
-  JGE: 0b011,
-  JLT: 0b100,
-  JNE: 0b101,
-  JLE: 0b110,
-  JMP: 0b111,
-} as const;
 
 const isValidDestCode = (code: string): code is keyof typeof DEST_CODES => Object.keys(DEST_CODES).includes(code);
 const isValidCompCode = (code: string): code is keyof typeof COMP_CODES => Object.keys(COMP_CODES).includes(code);
 const isValidJumpCode = (code: string): code is keyof typeof JUMP_CODES => Object.keys(JUMP_CODES).includes(code);
 const isMemoryCompCode = (code: keyof typeof COMP_CODES) => code.includes("M");
+
+const isEmptyLine = (line: string) => /^\s*$/.test(line);
+const isComment = (line: string) => line.startsWith("//");
+const isLabel = (line: string) => line.startsWith("(");
+const isInstruction = (line: string) => !(isEmptyLine(line) || isComment(line) || isLabel(line));
+
+const error = ({ message, lineNumber }: { message: string; lineNumber?: number }) =>
+  `error ${lineNumber !== undefined ? `(L${lineNumber}):` : ":"} ${message}`;
 
 const getAssemblyFilePath = (): Result<{ filePath: string }> => {
   const filePath = process.argv[2];
@@ -89,12 +30,12 @@ const getAssemblyFilePath = (): Result<{ filePath: string }> => {
   if (!filePath) {
     return {
       success: false,
-      message: "error: missing program file path" + "\n" + "usage: pnpm assembler ./path/to/program.asm",
+      message: error({ message: "missing program file path" + "\n" + "usage: pnpm assembler ./path/to/program.asm" }),
     };
   }
 
   if (!filePath.endsWith(".asm")) {
-    return { success: false, message: `error: file type of ${filePath} is not .asm` };
+    return { success: false, message: error({ message: `file type of ${filePath} is not .asm` }) };
   }
 
   return { success: true, filePath };
@@ -108,12 +49,10 @@ const getAssemblyProgram = (filePath: string): Result<{ assemblyProgram: readonl
         .toString()
         .trim()
         .split("\n")
-        .map((line) => line.trim().replaceAll(" ", ""))
-        .filter((line) => !!line && !line.startsWith("//")),
-      // TODO: dont remove whitespace or comments and improve error messages to include line number
+        .map((line) => line.trim().replaceAll(" ", "")),
     };
   } catch {
-    return { success: false, message: `error: unable to read file ${filePath}` };
+    return { success: false, message: error({ message: `unable to read file ${filePath}` }) };
   }
 };
 
@@ -127,15 +66,15 @@ const getSymbolTable = (assemblyProgram: readonly string[]): Result<{ symbolTabl
       const label = line.replace("(", "").replace(")", "");
 
       if (Object.keys(PREDEFINED_SYMBOLS).includes(label)) {
-        return { success: false, message: `error: predefined symbol ${label} cannot be used as a label` };
+        return { success: false, message: error({ message: `predefined symbol ${label} cannot be used as a label` }) };
       }
       if (symbolTable.has(label)) {
-        return { success: false, message: `error: label ${label} is defined more than once` };
+        return { success: false, message: error({ message: `label ${label} is defined more than once` }) };
       }
 
       symbolTable.set(label, instructionCount + 1);
     } else {
-      instructionCount++;
+      isInstruction(line) && instructionCount++;
     }
   }
 
@@ -144,7 +83,6 @@ const getSymbolTable = (assemblyProgram: readonly string[]): Result<{ symbolTabl
 
 const aToHack = (a: number): string => a.toString(2).padStart(16, "0");
 
-// TODO: break down into separate functions for A/C
 const parse = ({
   assemblyProgram,
   symbolTable,
@@ -152,13 +90,18 @@ const parse = ({
   assemblyProgram: readonly string[];
   symbolTable: Map<string, number>;
 }): Result<{ hackInstructions: readonly string[] }> => {
-  const assemblyInstructions = assemblyProgram.filter((i) => !i.startsWith("("));
   const hackInstructions: string[] = [];
-  let nextVariableAddress = 16;
+  let nextVariableAddress = VARIABLE_ADDRESS_START;
 
-  for (const instruction of assemblyInstructions) {
-    if (instruction.startsWith("@")) {
-      const address = instruction.slice(1);
+  for (const [i, line] of assemblyProgram.entries()) {
+    const lineNumber = i + 1;
+
+    if (!isInstruction(line)) {
+      continue;
+    }
+
+    if (line.startsWith("@")) {
+      const address = line.slice(1);
 
       if (/^\d+$/.test(address)) {
         hackInstructions.push(aToHack(Number(address)));
@@ -175,10 +118,10 @@ const parse = ({
       }
     } else {
       let cInstruction = C_INSTRUCTION_PREFIX;
-      const destAndRest = instruction.split("=");
+      const destAndRest = line.split("=");
 
       if (destAndRest.length < 1 || destAndRest.length > 3) {
-        return { success: false, message: `error: instruction ${instruction} is invalid` };
+        return { success: false, message: error({ message: `instruction ${line} is invalid`, lineNumber }) };
       }
 
       if (destAndRest.length === 2) {
@@ -188,7 +131,10 @@ const parse = ({
           if (isValidDestCode(dest)) {
             cInstruction |= DEST_CODES[dest];
           } else {
-            return { success: false, message: `error: ${dest} is not a valid destination register` };
+            return {
+              success: false,
+              message: error({ message: `${dest} is not a valid destination register`, lineNumber }),
+            };
           }
         }
       }
@@ -197,11 +143,14 @@ const parse = ({
       const comp = compAndJump?.shift();
 
       if (!comp) {
-        return { success: false, message: `error: instruction ${instruction} is missing comp symbol` };
+        return {
+          success: false,
+          message: error({ message: `instruction ${line} is missing comp symbol`, lineNumber }),
+        };
       }
 
       if (!isValidCompCode(comp)) {
-        return { success: false, message: `error: ${comp} is not a valid comp symbol` };
+        return { success: false, message: error({ message: `${comp} is not a valid comp symbol`, lineNumber }) };
       }
 
       cInstruction |= COMP_CODES[comp];
@@ -214,7 +163,10 @@ const parse = ({
 
       if (jump) {
         if (!isValidJumpCode(jump)) {
-          return { success: false, message: `error: ${jump} is not a valid jump symbol` };
+          return {
+            success: false,
+            message: error({ message: `${jump} is not a valid jump symbol`, lineNumber }),
+          };
         }
 
         cInstruction |= JUMP_CODES[jump];
@@ -265,7 +217,7 @@ const main = () => {
   try {
     writeFileSync(hackFileName, hackInstructions.join("\n"));
   } catch {
-    console.log(`error: unable to write file ${hackFileName}`);
+    console.log(error({ message: `unable to write hack instructions to file ${hackFileName}` }));
   }
 };
 
