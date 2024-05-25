2b4fa31d78aa90e17b16720ac4540702c7fe0e1e
Thomas Cleary <thomasclearydev@gmail.com>
2024-05-25 17:45:11 +0800

    Project 11: Use top level index file for compiler command

--- projects/10-11/src/index.ts
+++ projects/10-11/src/index.ts
@@ -1,5 +1,56 @@
+import { JackCompiler } from "./compiler/JackCompiler.js";
+import { error } from "./utils/index.js";
+import { getJackFiles, toJackProgram } from "./utils/jackFileUtils.js";
+
 const main = () => {
-  throw new Error("Not implemented");
+  const filePath = process.argv[2];
+
+  if (!filePath) {
+    console.log(error("missing program file path" + "\n" + "usage: pnpm compiler ./path/to/program"));
+    return;
+  }
+
+  const jackFilesResult = getJackFiles(filePath);
+
+  if (!jackFilesResult.success) {
+    console.log(jackFilesResult.message);
+    return;
+  }
+
+  const { jackFiles } = jackFilesResult;
+  const jackCompiler = new JackCompiler();
+
+  for (const file of jackFiles) {
+    const jackProgramResult = toJackProgram(file);
+
+    if (!jackProgramResult.success) {
+      console.log(jackProgramResult.message);
+      return;
+    }
+
+    const { jackProgram } = jackProgramResult;
+
+    const compilationResult = jackCompiler.compile(jackProgram);
+
+    if (!compilationResult.success) {
+      console.log(compilationResult.message);
+      return;
+    }
+
+    const { vmInstructions } = compilationResult;
+
+    console.log("VM Instructions");
+    console.log(vmInstructions);
+
+    // TODO: output compilation result to .vm file
+    // const outfile = `${filePath}/${path.basename(file).replace(".jack", ".vm")}`;
+
+    // try {
+    //   writeFileSync(outfile, "TODO");
+    // } catch {
+    //   console.log(error(`unable to test output to file ${outfile}`));
+    // }
+  }
 };
 
 main();
