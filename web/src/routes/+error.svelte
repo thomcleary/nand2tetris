028a8f15acb22b0f6e02151e7083b5d591ef61b5
thomcleary <thomasclearydev@gmail.com>
2024-06-23 00:11:46 +0800

    web: okay no, i think i was on to something

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	// adapter-static 'fallback' setup to return 404.html
+	if (browser && $page.status === 404) {
+		goto('./');
+	}
+</script>
