6cb3865c9ba4f9e64654878ef7db983bfb2c6ea3
thomcleary <thomasclearydev@gmail.com>
2024-06-22 23:29:57 +0800

    web: deploy?

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -8,6 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: '/nand2tetris' // Needs to match repo name to deploy to github pages (see: https://kit.svelte.dev/docs/adapter-static#github-pages)
+		},
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
